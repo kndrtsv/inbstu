7d046a409a01c3b2bf83cb364557ce21b9c6275f
kndrtsv <kndrtsv@gmail.com>
2025-05-29 01:52:31 +0300

    added Report.h

---
+++ oop/coursework/Report.h
@@ -0,0 +1,12 @@
+#ifndef COURSEWORK_REPORT_H
+#define COURSEWORK_REPORT_H
+
+#include <map>
+#include <string>
+
+class Report {
+public:
+    std::map<std::string, double> totals;
+};
+
+#endif //COURSEWORK_REPORT_H
