3ae64a252ea9210448c82e9c099fc8ab60b3d546
kndrtsv <kndrtsv@gmail.com>
2025-05-29 03:37:17 +0300

    added getDate and getDescription into Transaction.cpp

--- oop/coursework/Transaction.cpp
+++ oop/coursework/Transaction.cpp
@@ -15,6 +15,14 @@ Transaction::Type Transaction::getType() const {
     return type;
 }
 
+const std::string& Transaction::getDate() const {
+    return date;
+}
+
+const std::string& Transaction::getDescription() const {
+    return description;
+}
+
 Income::Income(double amount, const std::string& date, const std::string& description, const std::string& category)
         : Transaction(amount, date, description, category, Transaction::INCOME) {}
 
